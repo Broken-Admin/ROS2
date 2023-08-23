ed600c79ff514bb1527657d95e90806575ca8e49
Alexander Resurreccion <brokengaming50@gmail.com>
2023-08-23 12:03:08 -0500

    Add ROS 2 base install script

---
+++ ROS_2_humble_base_install.sh
@@ -0,0 +1,31 @@
+# Check language locale
+lang_locale=$(locale | grep "LANG=")
+anticipated_locale="LANG=en_US.UTF-8"
+
+# Assign and install locale if there is an issue
+if [ $lang_locale != $anticipated_locale ]; then
+    sudo apt update && sudo apt install locales
+    sudo locale-gen en_US en_US.UTF-8
+    sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+    export LANG=en_US.UTF-8
+    echo "----------------------"
+    echo "Locale has been updated"
+fi
+
+# Add sources
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+# ROS 2 specific, GPG key for the repository
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+# Add the ROS 2 repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# Update package repository data and upgrade packages
+sudo apt update && sudo apt upgrade
+
+# Install ROS 2
+sudo apt install ros-humble-ros-base ros-dev-tools
+
+echo "Installed!"
